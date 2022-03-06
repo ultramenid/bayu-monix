3a3656635441057ab1866f19b4d68efb70975eff
ultramenid <malichamdan@gmail.com>
2022-03-06 20:50:07 +0700

    first init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': ['Lucyrose', 'Helvetica', 'Arial', 'sans-serif'],
+      'roboto' : ['Roboto']
+    }
+  },
+  plugins: [],
+}
